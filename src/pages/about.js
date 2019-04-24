403aec6610b1f792ecf2fb15738b2c10eb87d1ed
Neil Berg <neil.berg14@gmail.com>
2019-04-24 10:45:01 -0700

    building out about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,25 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Avatar from "../images/isabelle-wedding-pic.jpg"
+
+const Img = styled.img`
+  display: block;
+  width: 300px;
+  height: auto;
+  margin: 0 auto;
+`
 
 const About = ({ location }) => {
   return (
     <Layout location={location}>
       <SEO title="About" />
-      <h2>About Page</h2>
+      <Img src={Avatar} alt="Profile Headshot of Neil Berg" />
+      <h3>Bio</h3>
+      <h3>CV</h3>
+      <h3>Values</h3>
     </Layout>
   )
 }
