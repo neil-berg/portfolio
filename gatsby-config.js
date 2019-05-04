032b71f69a3a52c66ade1567a2c79279452b096f
Neil Berg <neil.berg14@gmail.com>
2019-05-04 16:57:07 -0700

    remove image links in blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 800,
+              linkImagesToOriginal: false,
             },
           },
           {
