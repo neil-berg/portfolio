db38cf9ae8bd4832cee14d7237f1fb3fdd3199da
Neil Berg <neil.berg14@gmail.com>
2019-05-04 13:42:03 -0700

    Custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,15 @@
 import React from "react"
 
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Header from "../components/header"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
+const NotFoundPage = ({ location }) => (
+  <div>
+    <SEO title="Not found" />
+    <Header location={location} />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <p>{location.pathname.split("/")[1]} is not found...</p>
+  </div>
 )
 
 export default NotFoundPage
