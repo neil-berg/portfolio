d9be0930129a4a7397ba8e1b5aa6470e03e97f0c
Neil Berg <neil.berg14@gmail.com>
2020-01-14 11:18:01 -0800

    feat: add new tech skills section

---
+++ src/data/skills.js
@@ -0,0 +1,14 @@
+export const skills = {
+    "Languages": ["JavaScript, Python, HTML/CSS, SQL, bash/linux"],
+    "JS Frameworks & Libraries": ["React, Node, Redux, Gatsby, Express, Koa"],
+    "Database": ["PostgreSQL, MongoDB, Firebase, Mongoose, Knex"],
+    "Cloud": ["AWS (S3, EC2), Google StackDriver, Google Big Query"],
+    "Project": ["Agile, TDD, BDD, Git/GitHub"],
+    "Visualizations": ["Data Studio, D3, ReCharts"],
+    "API": ["REST, GraphQL, Postman"],
+    "Styling": ["Styled-components, React-spring, Bootstrap"],
+    "Building": ["Webpack, Babel"],
+    "Hosting": ["Netlify, Heroku"],
+    "CMS": ["Contentful, Airtable"],
+    "Python Modules": ["Pandas, Numpy, Matplotlib, Xarray"]
+}
